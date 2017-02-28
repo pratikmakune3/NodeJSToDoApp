394aedaf8922bf77729bc50ed5b7da7f862b8aeb
Prataik <Prataik@Sharans-MacBook-Pro.local>
2017-02-28 14:41:32 +0530

    Handling get Request and Rendering todo.ejs

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,7 +1,8 @@
 module.exports = function(app){
-  
-  app.get('/todo', function(req, res){
 
+  //Request Handler
+  app.get('/todo', function(req, res){
+    res.render('todo');
   });
 
   app.post('/todo', function(req, res){
