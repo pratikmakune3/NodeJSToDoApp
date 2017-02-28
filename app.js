5fd9059eca7d7e0ded8784f87b6b6f59ec293bbd
Prataik <Prataik@Sharans-MacBook-Pro.local>
2017-02-28 13:48:29 +0530

    MVC - Adding Controller

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var todoController = require('./controllers/todoController')
 
 var app = express();
 
@@ -7,6 +8,9 @@ app.set('view engine', 'ejs');
 
 //static files
 app.use(express.static('./public'));
+
+//fire controller
+todoController(app);
 
 //listen to port 3000
 app.listen(3000);
